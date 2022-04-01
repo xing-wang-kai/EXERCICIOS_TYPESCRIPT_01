694de49f549182e16beb66a5e4585409ea18532c
Kai Wang <kaiuchihaa@gmail.com>
2022-04-01 00:26:38 -0300

    editado o gitignore para mostrar a pasta dist com o retorno js

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,32 +1,24 @@
 import Negociacao from "../models/negociacao.js";
-
 const $ = window.document.querySelector.bind(document);
-
 export class NegociacaoController {
-    private inputData: HTMLFormElement;
-    private inputQuantidade: HTMLFormElement;
-    private inputValor: HTMLFormElement;
-
-    constructor(){
-        this.inputData = $("#data") as HTMLFormElement;
-        this.inputQuantidade = $("#quantidade")as HTMLFormElement;
-        this.inputValor = $("#valor")as HTMLFormElement;
+    constructor() {
+        this.inputData = $("#data");
+        this.inputQuantidade = $("#quantidade");
+        this.inputValor = $("#valor");
     }
-    adicionar(): void{
+    adicionar() {
         const negociacao = this.criarNegociacao;
         console.log(negociacao);
         this.limparFormulario;
-
     }
-    criarNegociacao():Negociacao {
+    criarNegociacao() {
         const regex = /-/g;
         const date = new Date(this.inputData.value.replace(regex, ","));
         const valor = Number.parseInt(this.inputValor.value);
         const quantidade = Number.parseFloat(this.inputQuantidade.value);
-
-        return new Negociacao( valor, date, quantidade );
+        return new Negociacao(valor, date, quantidade);
     }
-    limparFormulario(): void {
+    limparFormulario() {
         this.inputData.value = "";
         this.inputQuantidade.value = "";
         this.inputValor.value = "";
